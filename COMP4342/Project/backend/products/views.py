9f7c0e6df4666d006213aa672924323ecb985233
Zoe Zhou <siyuzhou2003@outlook.com>
2024-04-16 16:16:17 +0800

    Update COMP4342

--- COMP4342/Project/backend/products/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Product
-from .ProductSerializer import ProductSerializer
-from rest_framework.response import Response
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
--- COMP4342/Project/run_program.bat
+++
@@ -1,2 +0,0 @@
-@REM start cmd /c "npm install"
-start cmd /c "cd frontend && npx expo start --web"
