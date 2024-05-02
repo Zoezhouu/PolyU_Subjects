5afb4d6ea07b5b4c05417ef254ab0e8639b172da
Zoe Zhou <siyuzhou2003@outlook.com>
2024-05-03 01:01:11 +0800

    Update COMP3334

--- COMP3334/Lecture Exercise code/Lec2 - Ceasar cipher.py
+++ COMP3334/Lecture Exercise code/Lec2 - Ceasar cipher.py
@@ -1,9 +1,9 @@
 # Fix this encryption function, it does not encrypt correctly!
 def encrypt(letter):
-    return chr(ord(letter)+3)
+    return chr((ord(letter) - ord('A') +3) % 26 + ord('A'))
 # Add a decription function!
 def decrypt(letter):
-    return chr(ord(letter)-3) #fix here
+    return chr((ord(letter)- ord('A') -3) % 26 + ord('A')) #fix here
 
 print(''.join([encrypt(l) for l in list("HELLO")]))
 print(''.join([decrypt(l) for l in list("KHOOR")]))
